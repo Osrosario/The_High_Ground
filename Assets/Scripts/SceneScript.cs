df8613a230f153b841018b86c2dd7ba9f2db40e0
Osrosario <114501083+Osrosario@users.noreply.github.com>
2023-02-04 12:07:18 -0500

    Updated Project Settings

--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -6,28 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneScript : MonoBehaviour
 {
-    //a new variable that you can change in the inspector
-    public string nextLevel;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //a public method that will run when you click the button
+    public int SceneIndex;
+    
     public void ButtonToChangeLevel()
     {
-
         //loads the scene written in the inspector
-        SceneManager.LoadScene(nextLevel);
-
+        SceneManager.LoadScene(SceneIndex);
     }
 }
